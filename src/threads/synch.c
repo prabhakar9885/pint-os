56c2311eb3da090d1a1f8bb138c187e5bfe0a946
prabhakar9885 <prabhakar.9885@gmail.com>
2015-11-07 23:34:30 +0530

    Implemented Priority-semaphore.

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -114,9 +114,10 @@ sema_up (struct semaphore *sema)
 
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
+    thread_unblock (list_entry (get_high_priority_element (&sema->waiters),
                                 struct thread, elem));
   sema->value++;
+  thread_yield();
   intr_set_level (old_level);
 }
 
