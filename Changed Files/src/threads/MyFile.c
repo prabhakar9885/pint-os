f02b2f283ef2a98e60623c4afa9b2b7184107348
prabhakar9885 <prabhakar.9885@gmail.com>
2015-12-17 09:55:54 +0530

    Grouped the edited files into folder Changed-files

---
+++ Changed Files/src/threads/MyFile.c
@@ -0,0 +1,15 @@
+#include "tests/threads/MyFile.h"
+#include <debug.h>
+#include <string.h>
+#include <stdio.h>
+
+/* Prints the welcome message. 
+   Invoked by the command: pintos hello
+ */
+
+void
+run_hello () 
+{
+    printf("*** Hello PintOS ***\n");
+}
+
